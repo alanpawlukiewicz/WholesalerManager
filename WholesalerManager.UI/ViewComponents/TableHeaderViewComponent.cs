7c37c100c08d9fb007ed8da6d335d8da934a33b9
alanpawlukiewicz <alan.pawlukiewicz@gmail.com>
2026-05-18 19:48:29 +0200

    updated Customers GUI, turned new table headers into view component

---
+++ WholesalerManager.UI/ViewComponents/TableHeaderViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WholesalerManager.Core.Enums;
+
+namespace WholesalerManager.UI.ViewComponents
+{
+    public class TableHeaderViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(string propertyName)
+        {
+            ViewBag.PropertyName = propertyName;
+            //ViewBag.SortOrder = sortOrder;
+            return View();
+        }
+    }
+}
